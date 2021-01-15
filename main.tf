a0a41345e4e90a09b144ef5ae0003dcd5c481f70
nfrappart <61788760+nfrappart@users.noreply.github.com>
2021-01-15 15:24:32 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,19 @@
+####################################################################
+# This module allows the creation of a Network Security Group Rule #
+####################################################################
+
+# creation of the rule
+
+resource "azurerm_network_security_rule" "Terra-NSGRule" {
+  name                         = var.NSGRuleName
+  priority                     = var.NSGRulePriority
+  direction                    = var.NSGRuleDirection
+  access                       = var.NSGRuleAccess
+  protocol                     = var.NSGRuleProtocol
+  source_port_range            = var.NSGRuleSourcePortRange
+  destination_port_ranges      = var.NSGRuleDestinationPortRanges
+  source_address_prefixes      = var.NSGRuleSourceAddressPrefixes
+  destination_address_prefixes = var.NSGRuleDestinationAddressPrefixes
+  resource_group_name          = var.RgName
+  network_security_group_name  = var.NSGReference
+}
